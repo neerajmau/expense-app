919fddd4d09ec77060493b6b0feb371c31b93d0c
neeraj <neeraj.maurya@innovationm.com>
2022-08-21 22:25:57 +0530

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+
 require("./db/mongoose");
 // const bcrypt = require("bcryptjs");
 // const userRouter = require("./router/user");
